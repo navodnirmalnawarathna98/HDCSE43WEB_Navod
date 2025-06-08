91fbd34e66a1e60567360813d2210d68e9563254
Navod Nirmal Nawarathna <71627414+navodnirmalnawarathna98@users.noreply.github.com>
2025-06-08 11:01:10 +0000

    update

---
+++ 2025_05_08/javascript/first.js
@@ -0,0 +1,2 @@
+document.getElementById("demo2")
+.innerHTML = "external javascript";
---
+++ 2025_05_08/javascript/variables.js
@@ -0,0 +1,48 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+
+
+// When to Use var, let, or const?
+// 1. Always declare variables
+
+// 2. Always use const if the value should not be changed
+
+// 3. Always use const if the type should not be changed (Arrays and Objects)
+
+// 4. Only use let if you can't use const
+
+// 5. Only use var if you MUST support old browsers.
+
+// Automatically
+x = 2;
+y = 3;
+
+z=x+y;
+
+// document.getElementById("demo").innerHTML = "The value of z is: " + z;
+
+
+var a = 5;
+var b = 6;
+var c = a + b;
+
+// document.getElementById("demo").innerHTML = "The value of c is: " + c;
+
+let t = 10;
+let s =20;
+let q = t*s;
+
+document.getElementById("demo").innerHTML = "The value of q is: q " + q;
+
+
+const p = 5;
+const l = 6;
+const g = p + l;
+
+
+
+
